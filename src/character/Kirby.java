65c835fe5c83f2bfaf02733eb20dd3896ceaefa4
LauchlanMcDonald <lauchlan.mcdonald@brentwood.ca>
2015-10-28 13:12:15 -0700

    update

--- src/character/Kirby.java
+++ src/character/Kirby.java
@@ -7,6 +7,7 @@ package character;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 /**
  *
@@ -16,35 +17,45 @@ public class Kirby {
 
     public void draw(Graphics graphics) {
         graphics.drawRect(x, y, width, height);
-        
-        
+
         //body
-        graphics.setColor(new Color(246, 96, 171));
+        graphics.setColor(new Color(255, 105, 180, 190));
         graphics.fillOval(x, y, width, height);
-        
+
         //eyesblack
         graphics.setColor(new Color(0, 0, 0));
         graphics.fillOval(x + (width * 37 / 100), y + (height * 30 / 100), width * 10 / 100, height * 20 / 100);
         graphics.setColor(new Color(0, 0, 0));
         graphics.fillOval(x + (width * 55 / 100), y + (height * 30 / 100), width * 10 / 100, height * 20 / 100);
-        
+
         //eyesblue
-        graphics.setColor(new Color(0,0,205));
+        graphics.setColor(new Color(0, 0, 205));
         graphics.fillOval(x + (width * 38 / 100), y + (height * 31 / 100), width * 7 / 100, height * 17 / 100);
-        graphics.setColor(new Color(0,0,205));
+        graphics.setColor(new Color(0, 0, 205));
         graphics.fillOval(x + (width * 56 / 100), y + (height * 31 / 100), width * 7 / 100, height * 17 / 100);
-        
+
         //eyeswhite
-        graphics.setColor(new Color(255,255,255));
+        graphics.setColor(new Color(255, 255, 255));
         graphics.fillOval(x + (width * 39 / 100), y + (height * 31 / 100), width * 4 / 100, height * 6 / 100);
-        graphics.setColor(new Color(255,255,255));
+        graphics.setColor(new Color(255, 255, 255));
         graphics.fillOval(x + (width * 57 / 100), y + (height * 31 / 100), width * 4 / 100, height * 6 / 100);
-        
-        //feet
-        
-        
+
+        //cheeks
+        graphics.setColor(new Color(255, 20, 147, 85));
+        graphics.fillOval(x + (width * 18 / 100), y + (height * 48 / 100), width * 15 / 100, height * 7 / 100);
+        graphics.setColor(new Color(255, 20, 147, 85));
+        graphics.fillOval(x + (width * 69 / 100), y + (height * 48 / 100), width * 15 / 100, height * 7 / 100);
+
+        //mouthbackround
+        graphics.setColor(new Color(139, 0, 0));
+        graphics.fillOval(x + (width * 425 / 1000), y + (height * 55 / 100), width * 17 / 100, height * 14 / 100);
+
+        //mouthtonge
+        graphics.setColor(new Color(220, 20, 60));
+        graphics.fillArc(x + (width * 425 / 1000), y + (height * 605 / 1000), width * 17 / 100, height * 17 / 100, 30, 110);
+
     }
-    
+
     public Kirby(int x, int y, int height, int width) {
         this.x = x;
         this.y = y;
@@ -56,7 +67,5 @@ public class Kirby {
     private int y;
     private int height;
     private int width;
-    
-   
-    
+
 }
