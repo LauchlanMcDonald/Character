779a8fea2cf41c54bb706a55eca24bd8e6e2c4db
LauchlanMcDonald <lauchlan.mcdonald@brentwood.ca>
2015-11-03 10:10:25 -0800

    i dont know

--- src/character/Kirby.java
+++ src/character/Kirby.java
@@ -15,12 +15,27 @@ import java.awt.Graphics2D;
  */
 public class Kirby {
 
+    private static Color KIRBY_PINK = new Color(255, 105, 180);
+
     public void draw(Graphics graphics) {
         graphics.drawRect(x, y, width, height);
 
+        //feetbehind
+        graphics.setColor(new Color(220, 20, 60));
+        graphics.fillOval(x + (width * 13 / 100), y + (height * 65 / 100), width * 20 / 100, height * 40 / 100);
+        graphics.setColor(new Color(220, 20, 60));
+        graphics.fillOval(x + (width * 67 / 100), y + (height * 65 / 100), width * 20 / 100, height * 40 / 100);
+
+        //arms
+        graphics.setColor(KIRBY_PINK);
+        graphics.fillOval(x - (width * 10 / 100), y + (height * 30 / 100), width * 40 / 100, height * 20 / 100);
+        graphics.fillOval(x + (width * 71 / 100), y + (height * 30 / 100), width * 40 / 100, height * 20 / 100);
+
         //body
-        graphics.setColor(new Color(255, 105, 180, 190));
+        graphics.setColor(KIRBY_PINK);
         graphics.fillOval(x, y, width, height);
+        graphics.setColor(Color.BLACK);
+        graphics.drawOval(x, y, width, height);
 
         //eyesblack
         graphics.setColor(new Color(0, 0, 0));
@@ -50,9 +65,11 @@ public class Kirby {
         graphics.setColor(new Color(139, 0, 0));
         graphics.fillOval(x + (width * 425 / 1000), y + (height * 55 / 100), width * 17 / 100, height * 14 / 100);
 
-        //mouthtonge
+        //mouthtongue
         graphics.setColor(new Color(220, 20, 60));
-        graphics.fillArc(x + (width * 425 / 1000), y + (height * 605 / 1000), width * 17 / 100, height * 17 / 100, 30, 110);
+        graphics.fillOval(x + (width * 437 / 1000), y + (height * 59 / 100), width * 15 / 100, height * 10 / 100);
+        graphics.setColor(new Color(249, 0, 0));
+        graphics.fillOval(x + (width * 447 / 1000), y + (height * 61 / 100), width * 13 / 100, height * 8 / 100);
 
     }
 
